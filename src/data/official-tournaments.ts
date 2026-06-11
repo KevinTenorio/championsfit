f3743c35e1cd0a95a1e6d16ee1f0cd9426e80813
KevinTenorio <kevin.tenorio98@gmail.com>
2026-06-10 23:55:14 -0300

    feat: Adiciona as nationals na lista de torneios oficiais

--- src/data/official-tournaments.ts
+++ src/data/official-tournaments.ts
@@ -1,6 +1,11 @@
 export const OFFICIAL_TOURNAMENTS: ReadonlySet<string> = new Set([
   "Turin SPE 2026",
   "Indianapolis Regional 2026",
+  "PJCS 2026",
+  "Singapore MBL 2026",
+  "Korea PTC 2026",
+  "Thailand MBL 2026",
+  "Malaysia MBL 2026"
 ]);
 
 export function isOfficialTournament(name: string): boolean {
